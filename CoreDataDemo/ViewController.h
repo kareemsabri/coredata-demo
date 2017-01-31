cf292be6a66e381f5659fe9db8d33fc1f9cafd20
Kareem Sabri <kareem.sabri@gmail.com>
2017-01-31 17:56:33 -0500

    clean state

---
+++ CoreDataDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataDemo
+//
+//  Created by Kareem Sabri on 2017-01-31.
+//  Copyright © 2017 Kareem Sabri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreDataDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CoreDataDemo
+//
+//  Created by Kareem Sabri on 2017-01-31.
+//  Copyright © 2017 Kareem Sabri. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
